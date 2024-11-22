c62c4f8b7899226f3a24ff3f28fafc424b284a45
Administrador <josemariazapata12@gmail.com>
2024-11-21 21:48:21 -0400

    scrollbar

--- js/AutoScroll.js
+++ js/AutoScroll.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script cargado correctamente');
+
     const scrollContainer = document.querySelector('.team-scroll');
-    const scrollSpeed =2; 
+    const scrollSpeed = 2; 
+
+    function appendClones() {
+        const children = [...scrollContainer.children];
+        children.forEach(child => {
+            const clone = child.cloneNode(true);
+            scrollContainer.appendChild(clone);
+        });
+    }
+    appendClones();
 
     function autoScroll() {
         scrollContainer.scrollLeft += scrollSpeed;
-        if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth / 2) {
-            scrollContainer.scrollLeft = 0;
+
+        if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+            appendClones(); 
         }
 
         requestAnimationFrame(autoScroll);
     }
-    const children = [...scrollContainer.children];
-    children.forEach(child => {
-        const clone = child.cloneNode(true);
-        scrollContainer.appendChild(clone);
-    });
 
     autoScroll();
 });
